c5c75844b31e820fa3bafb74fbd4e93e021f7189
ytouati <ytouati@divalto.com>
2021-01-19 21:43:20 +0100

    setting up Facebook SDK

--- API/views/templates/home.php
+++ API/views/templates/home.php
@@ -59,6 +59,29 @@
 
     <body>
 
+        <!-- loading Facebook SDK -->
+        <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+            appId      : '<?=$_ENV["FACEBOOK_APP_ID"];?>',
+            cookie     : true,
+            xfbml      : true,
+            version    : '<?=$_ENV["CURRENT_FACEBOOK_API_VERSION"];?>'
+            });
+            
+            FB.AppEvents.logPageView();   
+            
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        </script>
+
         <!-- SO template -->
         <div class="contentmainbody2">
 
