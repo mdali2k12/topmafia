48c94a6552addaaf5f6f2c98ed83e84fce827809
ytouati <ytouati@divalto.com>
2021-01-28 22:54:04 +0100

    footer migrated to component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './components/slideshow/slideshow.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
